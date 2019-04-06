eec53da3e4e748a618601506a70625b5dcd31a14
Matthew R. Norman <normanmr@ornl.gov>
2019-04-06 19:26:15 -0400

    A bit of code cleanup

--- TimeIntegrator.h
+++ TimeIntegrator.h
@@ -27,22 +27,18 @@ public :
 
   inline void stepForwardSSPRK3(State &state, Domain &dom, Exchange &exch, Parallel &par) {
     tend.compEulerTendSD_X(state.state, dom, exch, par);
-    for (int l=0; l<numState; l++) {
-      for (int k=0; k<dom.nz; k++) {
-        for (int j=0; j<dom.ny; j++) {
-          for (int i=0; i<dom.nx; i++) {
-            state.state(l,hs+k,hs+j,hs+i) += dom.dt * tend.tend(l,k,j,i);
-          }
-        }
-      }
-    }
+    applyTendencies(state.state, dom, tend.tend);
 
     tend.compEulerTendSD_Y(state.state, dom, exch, par);
+    applyTendencies(state.state, dom, tend.tend);
+  }
+
+  inline void applyTendencies(Array<real> &state, Domain const &dom, Array<real> const &tend) {
     for (int l=0; l<numState; l++) {
       for (int k=0; k<dom.nz; k++) {
         for (int j=0; j<dom.ny; j++) {
           for (int i=0; i<dom.nx; i++) {
-            state.state(l,hs+k,hs+j,hs+i) += dom.dt * tend.tend(l,k,j,i);
+            state(l,hs+k,hs+j,hs+i) += dom.dt * tend(l,k,j,i);
           }
         }
       }
