71c11d17f0415be905a224ba0047ab82cb0b9c64
Matthew R. Norman <normanmr@ornl.gov>
2019-04-09 17:02:57 -0400

    Making Riemann solvers more modular

--- Tendencies.h
+++ Tendencies.h
@@ -6,6 +6,7 @@
 #include "Parallel.h"
 #include "SArray.h"
 #include "Array.h"
+#include "Riemann.h"
 #include "Domain.h"
 #include "Exchange.h"
 #include "TransformMatrices.h"
@@ -16,6 +17,7 @@ class Tendencies {
   Array<real> fluxLimits;
   Array<real> flux;
   TransformMatrices<real> trans;
+  Riemann riem;
 
 public :
 
@@ -110,61 +112,17 @@ public :
     for (int k=0; k<dom.nz; k++) {
       for (int j=0; j<dom.ny; j++) {
         for (int i=0; i<dom.nx+1; i++) {
-          SArray<real,numState> s1, s2, f1, f2, ch1, ch2, chu, ev;
-
-          // Store into local arrays
+          SArray<real,numState> s1, s2, f1, f2, upw;
           for (int l=0; l<numState; l++) {
             s1(l) = stateLimits(l,0,k,j,i);
             s2(l) = stateLimits(l,1,k,j,i);
             f1(l) = fluxLimits (l,0,k,j,i);
             f2(l) = fluxLimits (l,1,k,j,i);
           }
-
-          // Compute interface values
-          real r = 0.5_fp * ( s1(idR ) + s2(idR ) );
-          real u = 0.5_fp * ( s1(idRU) + s2(idRU) ) / r;
-          real v = 0.5_fp * ( s1(idRV) + s2(idRV) ) / r;
-          real w = 0.5_fp * ( s1(idRW) + s2(idRW) ) / r;
-          real t = 0.5_fp * ( s1(idTH) + s2(idTH) ) / r;
-          real p = C0 * mypow( r*t , GAMMA );
-          real cs = mysqrt( GAMMA * p / r );
-
-          // Compute left and right characteristic variables
-          ch1(0) =  u*f1(0)/(2*cs) - f1(1)/(2*cs) + f1(4)/(2*t);
-          ch1(1) = -u*f1(0)/(2*cs) + f1(1)/(2*cs) + f1(4)/(2*t);
-          ch1(2) = f1(0) -   f1(4)/t;
-          ch1(3) = f1(2) - v*f1(4)/t;
-          ch1(4) = f1(3) - w*f1(4)/t;
-
-          ch2(0) =  u*f2(0)/(2*cs) - f2(1)/(2*cs) + f2(4)/(2*t);
-          ch2(1) = -u*f2(0)/(2*cs) + f2(1)/(2*cs) + f2(4)/(2*t);
-          ch2(2) = f2(0) -   f2(4)/t;
-          ch2(3) = f2(2) - v*f2(4)/t;
-          ch2(4) = f2(3) - w*f2(4)/t;
-
-          ev(0) = u-cs;
-          ev(1) = u+cs;
-          ev(2) = u;
-          ev(3) = u;
-          ev(4) = u;
-
-          // Compute the upwind characteristics
+          riem.riemannX(s1, s2, f1, f2, upw);
           for (int l=0; l<numState; l++) {
-            if        (ev(l) > 0._fp) {
-              chu(l) = ch1(l);
-            } else if (ev(l) < 0._fp) {
-              chu(l) = ch2(l);
-            } else {
-              chu(l) = 0.5_fp * (ch1(l) + ch2(l));
-            }
+            flux(l,k,j,i) = upw(l);
           }
-
-          // Compute the fluxes
-          flux(0,k,j,i) = chu(0) + chu(1) + chu(2);
-          flux(1,k,j,i) = (u-cs)*chu(0) + (u+cs)*chu(1) + u*chu(2);
-          flux(2,k,j,i) = v*chu(0) + v*chu(1) + chu(3);
-          flux(3,k,j,i) = w*chu(0) + w*chu(1) + chu(4);
-          flux(4,k,j,i) = t*chu(0) + t*chu(1);
         }
       }
     }
@@ -265,61 +223,17 @@ public :
     for (int k=0; k<dom.nz; k++) {
       for (int j=0; j<dom.ny+1; j++) {
         for (int i=0; i<dom.nx; i++) {
-          SArray<real,numState> s1, s2, f1, f2, ch1, ch2, chu, ev;
-
-          // Store into local arrays
+          SArray<real,numState> s1, s2, f1, f2, upw;
           for (int l=0; l<numState; l++) {
             s1(l) = stateLimits(l,0,k,j,i);
             s2(l) = stateLimits(l,1,k,j,i);
             f1(l) = fluxLimits (l,0,k,j,i);
             f2(l) = fluxLimits (l,1,k,j,i);
           }
-
-          // Compute interface values
-          real r = 0.5_fp * ( s1(idR ) + s2(idR ) );
-          real u = 0.5_fp * ( s1(idRU) + s2(idRU) ) / r;
-          real v = 0.5_fp * ( s1(idRV) + s2(idRV) ) / r;
-          real w = 0.5_fp * ( s1(idRW) + s2(idRW) ) / r;
-          real t = 0.5_fp * ( s1(idTH) + s2(idTH) ) / r;
-          real p = C0 * mypow( r*t , GAMMA );
-          real cs = mysqrt( GAMMA * p / r );
-
-          // Compute left and right characteristic variables
-          ch1(0) =  v*f1(0)/(2*cs) - f1(2)/(2*cs) + f1(4)/(2*t);
-          ch1(1) = -v*f1(0)/(2*cs) + f1(2)/(2*cs) + f1(4)/(2*t);
-          ch1(2) = f1(0) -   f1(4)/t;
-          ch1(3) = f1(1) - u*f1(4)/t;
-          ch1(4) = f1(3) - w*f1(4)/t;
-
-          ch2(0) =  v*f2(0)/(2*cs) - f2(2)/(2*cs) + f2(4)/(2*t);
-          ch2(1) = -v*f2(0)/(2*cs) + f2(2)/(2*cs) + f2(4)/(2*t);
-          ch2(2) = f2(0) -   f2(4)/t;
-          ch2(3) = f2(1) - u*f2(4)/t;
-          ch2(4) = f2(3) - w*f2(4)/t;
-
-          ev(0) = v-cs;
-          ev(1) = v+cs;
-          ev(2) = v;
-          ev(3) = v;
-          ev(4) = v;
-
-          // Compute the upwind characteristics
+          riem.riemannY(s1, s2, f1, f2, upw);
           for (int l=0; l<numState; l++) {
-            if        (ev(l) > 0._fp) {
-              chu(l) = ch1(l);
-            } else if (ev(l) < 0._fp) {
-              chu(l) = ch2(l);
-            } else {
-              chu(l) = 0.5_fp * (ch1(l) + ch2(l));
-            }
+            flux(l,k,j,i) = upw(l);
           }
-
-          // Compute the fluxes
-          flux(0,k,j,i) = chu(0) + chu(1) + chu(2);
-          flux(1,k,j,i) = u*chu(0) + u*chu(1) + chu(3);
-          flux(2,k,j,i) = (v-cs)*chu(0) + (v+cs)*chu(1) + v*chu(2);
-          flux(3,k,j,i) = w*chu(0) + w*chu(1) + chu(4);
-          flux(4,k,j,i) = t*chu(0) + t*chu(1);
         }
       }
     }
@@ -460,61 +374,17 @@ public :
     for (int k=0; k<dom.nz+1; k++) {
       for (int j=0; j<dom.ny; j++) {
         for (int i=0; i<dom.nx; i++) {
-          SArray<real,numState> s1, s2, f1, f2, ch1, ch2, chu, ev;
-
-          // Store into local arrays
+          SArray<real,numState> s1, s2, f1, f2, upw;
           for (int l=0; l<numState; l++) {
             s1(l) = stateLimits(l,0,k,j,i);
             s2(l) = stateLimits(l,1,k,j,i);
             f1(l) = fluxLimits (l,0,k,j,i);
             f2(l) = fluxLimits (l,1,k,j,i);
           }
-
-          // Compute interface values
-          real r = 0.5_fp * ( s1(idR ) + s2(idR ) );
-          real u = 0.5_fp * ( s1(idRU) + s2(idRU) ) / r;
-          real v = 0.5_fp * ( s1(idRV) + s2(idRV) ) / r;
-          real w = 0.5_fp * ( s1(idRW) + s2(idRW) ) / r;
-          real t = 0.5_fp * ( s1(idTH) + s2(idTH) ) / r;
-          real p = C0 * mypow( r*t , GAMMA );
-          real cs = mysqrt( GAMMA * p / r );
-
-          // Compute left and right characteristic variables
-          ch1(0) =  w*f1(0)/(2*cs) - f1(3)/(2*cs) + f1(4)/(2*t);
-          ch1(1) = -w*f1(0)/(2*cs) + f1(3)/(2*cs) + f1(4)/(2*t);
-          ch1(2) = f1(0) -   f1(4)/t;
-          ch1(3) = f1(1) - u*f1(4)/t;
-          ch1(4) = f1(2) - v*f1(4)/t;
-
-          ch2(0) =  w*f2(0)/(2*cs) - f2(3)/(2*cs) + f2(4)/(2*t);
-          ch2(1) = -w*f2(0)/(2*cs) + f2(3)/(2*cs) + f2(4)/(2*t);
-          ch2(2) = f2(0) -   f2(4)/t;
-          ch2(3) = f2(1) - u*f2(4)/t;
-          ch2(4) = f2(2) - v*f2(4)/t;
-
-          ev(0) = w-cs;
-          ev(1) = w+cs;
-          ev(2) = w;
-          ev(3) = w;
-          ev(4) = w;
-
-          // Compute the upwind characteristics
+          riem.riemannZ(s1, s2, f1, f2, upw);
           for (int l=0; l<numState; l++) {
-            if        (ev(l) > 0._fp) {
-              chu(l) = ch1(l);
-            } else if (ev(l) < 0._fp) {
-              chu(l) = ch2(l);
-            } else {
-              chu(l) = 0.5_fp * (ch1(l) + ch2(l));
-            }
+            flux(l,k,j,i) = upw(l);
           }
-
-          // Compute the fluxes
-          flux(0,k,j,i) = chu(0) + chu(1) + chu(2);
-          flux(1,k,j,i) = u*chu(0) + u*chu(1) + chu(3);
-          flux(2,k,j,i) = v*chu(0) + v*chu(1) + chu(4);
-          flux(3,k,j,i) = (w-cs)*chu(0) + (w+cs)*chu(1) + w*chu(3);
-          flux(4,k,j,i) = t*chu(0) + t*chu(1);
         }
       }
     }
