c97450aadcf611737db339e5f618946f6b3e321e
Matt Norman <normanmr@ornl.gov>
2019-05-02 15:49:31 -0400

    I tried a lot of things with quasi-compressibility, and it never panned out. Moving on to characteristic splitting with Lagrangian characteristics.

--- const.h
+++ const.h
@@ -24,14 +24,13 @@ inline _HOSTDEV real operator"" _fp( long double x ) {
 #define tord 3
 int const hs = (ord-1)/2;
 
-int const numState = 6;
+int const numState = 5;
 
 int const idR  = 0;
 int const idRU = 1;
 int const idRV = 2;
 int const idRW = 3;
 int const idRT = 4;
-int const idRP = 5;
 
 // Some physical constants
 real const PI    = 3.1415926535897932384626433832795028842;
