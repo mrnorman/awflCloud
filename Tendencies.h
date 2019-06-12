5a1f78a3cf54e9132df055a7dafc6a5a0321e5e3
Matt Norman <normanmr@ornl.gov>
2019-06-12 07:31:31 -0400

    Revert "Easier to work with z-dimension first"
    This reverts commit 3f2005229db3fb3c3aa595877995f6458594e7d4.

--- Tendencies.h
+++ Tendencies.h
@@ -263,23 +263,32 @@ public :
   }
 
 
-  inline void compEulerTendSplitSD_Z(real4d &state, real2d const &hyDensGLL, real2d const &hyDensThetaGLL,
+  inline void compEulerTendSplitSD_X(real4d &state, real1d const &hyDensCells, real1d const &hyDensThetaCells,
                                      Domain const &dom, Exchange &exch, Parallel const &par, real4d &tend) {
 
-    // Boundaries for the fluid state in the z-direction
-    stateBoundariesZ(state, dom);
+
+    //Exchange halos in the x-direction
+    exch.haloInit      ();
+    exch.haloPackN_x   (dom, state, numState);
+    exch.haloExchange_x(dom, par);
+    exch.haloUnpackN_x (dom, state, numState);
 
     // Reconstruct to tord GLL points in the x-direction
-    reconSD_Z(state, hyDensGLL, hyDensThetaGLL, dom, wenoRecon, to_gll, stateLimits, fluxLimits, wenoIdl, wenoSigma);
+    reconSD_X(state, hyDensCells, hyDensThetaCells, dom, wenoRecon, to_gll, stateLimits, fluxLimits, wenoIdl, wenoSigma);
 
-    // Apply boundary conditions to fluxes and state values
-    edgeBoundariesZ(stateLimits, fluxLimits, dom);
+    //Reconcile the edge fluxes via MPI exchange.
+    exch.haloInit      ();
+    exch.edgePackN_x   (dom, stateLimits, numState);
+    exch.edgePackN_x   (dom, fluxLimits , numState);
+    exch.edgeExchange_x(dom, par);
+    exch.edgeUnpackN_x (dom, stateLimits, numState);
+    exch.edgeUnpackN_x (dom, fluxLimits , numState);
 
     // Riemann solver
-    computeFlux_Z(stateLimits, fluxLimits, flux, dom);
+    computeFlux_X(stateLimits, fluxLimits, flux, dom);
 
     // Form the tendencies
-    computeTend_Z(flux, tend, src, dom);
+    computeTend_X(flux, tend, dom);
   }
 
 
